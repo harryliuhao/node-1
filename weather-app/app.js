d6d9c55b6bfe9b1e3d7aa36f2c1852eddaabf17d
Hao Liu <harryliuhao@hotmail.com>
2021-05-30 20:19:09 -0400

    callback added

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,24 +1,11 @@
-const request=require('request')
+const geocode=require('./utils/geocode')
+const forecast=require('./utils/forecast')
 
-const placeInput='Oakton VA'
-// const weatherUrl='http://api.weatherstack.com/current?access_key=e3a4c5a4a3e28673e4aaf0706bf1aa69&query=38.8873249,-77.350638&units=f'
+const placeInput=process.argv[2]
 
-const mapUrl='https://api.mapbox.com/geocoding/v5/mapbox.places/'+placeInput+'.json?access_token=pk.eyJ1IjoiaGFycnlsaXVoYW8iLCJhIjoiY2twYTBucDNwMGxtYzJ2b2ZoZ2RxaTJqOSJ9.s9j22OFzmKPvJtca6Nhlig&limit=1'
+geocode(placeInput,(error,data)=>{
 
-// request({url: weatherUrl, json: true}, (error, response)=>{
-//     const loc=response.body.location.name
-//     const temp=response.body.current.temperature 
-//     const rainChance=response.body.current.precip*100
-//     const condition=response.body.current.weather_descriptions[0]
-//     console.log(condition+'. Current temperature at location',loc,'is', temp,'F and chance of rain is', rainChance,'%')
-// })
-
-request({url: mapUrl}, (error, response)=>{
-    
-    const body=JSON.parse(response.body)
-    const place=body.features[0].place_name
-    const longitude=body.features[0].center[0]
-    const latitude=body.features[0].center[1]
-
-    console.log('Coordinate for',place, 'is',longitude,',',latitude)
+    forecast(data.latitude,data.longitude,(error,weatherData)=>{
+        console.log('Current weather for',data.place+':',weatherData.condition+'.', weatherData.temp+'F and chance of rain is', weatherData.rainChance+'%')
+    })
 })
